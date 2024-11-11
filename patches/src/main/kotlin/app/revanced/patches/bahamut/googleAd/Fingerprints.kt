7e8d4555e2ba04e685f6adf3cc0518e0e2dabec3
nyraa <112930946+nyraa@users.noreply.github.com>
2024-11-11 15:43:48 +0800

    update fingerprint

--- patches/src/main/kotlin/app/revanced/patches/bahamut/googleAd/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/bahamut/googleAd/Fingerprints.kt
@@ -20,7 +20,7 @@ internal val googleAdLoader2Fingerprint = fingerprint {
 
 internal val googleAdLoader3Fingerprint = fingerprint {
     returns("V")
-    parameters("Lcom/google/android/gms/ads/AdRequest;I")
+    parameters("Lcom/google/android/gms/ads/AdRequest;", "I")
     custom { methodDef, classDef ->
         classDef.type == "Lcom/google/android/gms/ads/AdLoader;" && methodDef.name == "loadAds"
     }
